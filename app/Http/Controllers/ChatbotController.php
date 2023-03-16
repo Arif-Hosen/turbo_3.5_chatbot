f20547be72f08ee15872d75777503c95ec937c4a
arif_hosen <arif.hosen3888@gmail.com>
2023-03-17 01:22:01 +0600

    response data appned to container , handle error and train

--- app/Http/Controllers/ChatbotController.php
+++ app/Http/Controllers/ChatbotController.php
@@ -7,41 +7,54 @@ use Orhanerday\OpenAi\OpenAi;
 
 class ChatbotController extends Controller
 {
-    public function create(Request $request){
+    public function create(Request $request)
+    {
         
 
-        if($request->isMethod('POST')){
-            return ($request->inputValue);
+        // return json_decode($complete)->error->message;
+
+        if ($request->isMethod('POST')) {
+            // return ($request->inputValue);
             $open_ai_key = getenv('OPENAI_API_KEY');
-        $open_ai = new OpenAi($open_ai_key);
-        
-        $complete = $open_ai->chat([
-            'model' => 'gpt-3.5-turbo',
-            'messages' => [
-                [
-                    "role" => "system",
-                    "content" => "You are a helpful assistant of Zainik Labs."
+            $open_ai = new OpenAi($open_ai_key);
+
+            $userMessage = $request->inputValue;
+            $complete = $open_ai->chat([
+                'model' => 'gpt-3.5-turbo',
+                'messages' => [
+                    [
+                        "role" => "system",
+                        "content" => "You are a helpful assistant."
+                    ],
+                    [
+                        "role" => "user",
+                        "content" => "Tell me about Zainik Labs"
+                    ],
+                    [
+                        "role" => "assistant",
+                        "content" => "Zainik Lab started its journey in 2015 as an individual endeavor.Zainik Lab took the form of a full-fledged UI/UX agency."
+                    ],
+                    [
+                        "role" => "user",
+                        "content" => "Founder"
+                    ],
+                    [
+                        "role" => "assistant",
+                        "content" => "Shaharuzzaman Sourav"
+                    ],
+                    
+                    [
+                        "role" => "user",
+                        "content" => "$userMessage"
+                    ],
                 ],
-                [
-                    "role" => "user",
-                    "content" => "Tell me about Zainik Labs?"
-                ],
-                [
-                    "role" => "assistant",
-                    "content" => "Zainik Lab started its journey in 2015 as an individual endeavor. In 2017, Zainik Lab took the form of a full-fledged UI/UX agency."
-                ],
-                [
-                    "role" => "user",
-                    "content" => "What is your best product?"
-                ],
-            ],
-            'temperature' => 1.0,
-            'max_tokens' => 4000,
-            'frequency_penalty' => 0,
-            'presence_penalty' => 0,
-         ]);
-         
-         return json_decode($complete)?->choices[0]->message->content;
+                'temperature' => 1.0,
+                'max_tokens' => 4000,
+                'frequency_penalty' => 0,
+                'presence_penalty' => 0,
+            ]);
+
+            return json_decode($complete);
             // $open_ai = new OpenAi($open_ai_key);
         }
         return view('chatbot.index');
